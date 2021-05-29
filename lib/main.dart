f8c4d43bcec746ff83f45dd7079528412670877a
Cheng Po Sheng <bencer@outlook.com>
2021-05-30 01:07:00 +0800

    gallery animation done

--- lib/main.dart
+++ lib/main.dart
@@ -32,29 +32,61 @@ class AlbumGrid extends StatefulWidget {
 }
 
 class _AlbumGridState extends State<AlbumGrid> {
-  final ScrollController gridControl =
-      ScrollController(initialScrollOffset: -100, keepScrollOffset: true);
-
   List<double> _elevation = [];
+
+  Gradient linearGradient = SweepGradient(colors: [
+    Colors.grey.shade400,
+    Colors.green.shade200,
+    Colors.amber.shade200,
+    Colors.grey.shade400,
+  ], stops: [
+    0.1,
+    0.3,
+    0.8,
+    1.0
+  ], center: Alignment(-0.9, 0));
+
+  Gradient linearGradient3 = LinearGradient(colors: [
+    Colors.grey.shade400,
+    Colors.blueGrey.shade200,
+    Colors.red.shade200
+  ], stops: [
+    0.3,
+    0.8,
+    1.0
+  ]);
+
+  List<Color> _greenradius = [];
 
   @override
   Widget build(BuildContext context) {
     for (int i = 0; i < ntuclose.photosList().length; i++) {
       _elevation.add(16.0);
+      _greenradius.add(Colors.grey.shade400);
     }
 
     ntucloseSetStr();
+
     return Stack(children: [
       Container(
         color: Colors.grey.shade400,
         child: GridView.builder(
-          controller: gridControl,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: MediaQuery.of(context).size.width >
+                      MediaQuery.of(context).size.height
+                  ? 3
+                  : 2,
+              mainAxisSpacing: 0,
+              crossAxisSpacing: 10,
+              childAspectRatio: 1),
+          itemCount: ntuclose.photosList().length,
+          padding: EdgeInsets.only(top: 80),
           itemBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(children: [
                 AnimatedPhysicalModel(
-                    duration: Duration(milliseconds: 200),
+                    duration: Duration(milliseconds: 300),
                     shape: BoxShape.rectangle,
                     shadowColor: Colors.black,
                     color: Colors.transparent,
@@ -64,14 +96,17 @@ class _AlbumGridState extends State<AlbumGrid> {
                       onTapDown: (details) {
                         setState(() {
                           _elevation[index] = 0;
+                          _greenradius[index] = Colors.green.shade200;
                         });
-                        Timer(Duration(milliseconds: 300), () {
+                        Timer(Duration(milliseconds: 400), () {
                           setState(() {
                             _elevation[index] = 16;
+                            _greenradius[index] = Colors.grey.shade400;
                           });
                         });
                       },
-                      child: Container(
+                      child: AnimatedContainer(
+                          duration: Duration(milliseconds: 300),
                           height: MediaQuery.of(context).size.width /
                               (MediaQuery.of(context).size.width >
                                       MediaQuery.of(context).size.height
@@ -83,12 +118,18 @@ class _AlbumGridState extends State<AlbumGrid> {
                               image: ntuclose.photosList()[index].image,
                               fit: BoxFit.contain,
                             ),
-                            color: Colors.grey.shade400,
+                            gradient: RadialGradient(colors: [
+                              _greenradius[index],
+                              Colors.grey.shade400,
+                            ], stops: [
+                              0.8,
+                              1.0
+                            ]),
                             borderRadius: BorderRadius.circular(10),
                           )),
                     )),
                 AnimatedPhysicalModel(
-                  duration: Duration(milliseconds: 800),
+                  duration: Duration(milliseconds: 200),
                   shape: BoxShape.rectangle,
                   shadowColor: Colors.black,
                   color: Colors.transparent,
@@ -98,9 +139,7 @@ class _AlbumGridState extends State<AlbumGrid> {
                     margin: EdgeInsets.only(top: 5),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
-                      gradient: LinearGradient(
-                          colors: [Colors.blue.shade200, Colors.red.shade200],
-                          stops: [0.3, 1.0]),
+                      gradient: linearGradient,
                     ),
                     child: Align(
                       alignment: Alignment.bottomLeft,
@@ -109,7 +148,7 @@ class _AlbumGridState extends State<AlbumGrid> {
                         child: Text(
                           "${ntuclose.photos[index].des}",
                           style: TextStyle(
-                              fontSize: 10,
+                              fontSize: 12,
                               fontFamily: 'NotoSans',
                               color: Colors.black),
                         ),
@@ -120,15 +159,6 @@ class _AlbumGridState extends State<AlbumGrid> {
               ]),
             );
           },
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: MediaQuery.of(context).size.width >
-                      MediaQuery.of(context).size.height
-                  ? 3
-                  : 2,
-              mainAxisSpacing: 0,
-              crossAxisSpacing: 10,
-              childAspectRatio: 1),
-          itemCount: ntuclose.photosList().length,
         ),
       ),
       Align(
@@ -141,16 +171,14 @@ class _AlbumGridState extends State<AlbumGrid> {
             width: double.maxFinite,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-                  colors: [Colors.blue.shade200, Colors.red.shade200],
-                  stops: [0.3, 1.0]),
+              gradient: linearGradient3,
             ),
             margin: EdgeInsets.all(10),
             child: Padding(
               padding: const EdgeInsets.only(
                   top: 10.0, bottom: 10.0, right: 10, left: 100),
               child: Text(
-                "疫情三級警戒，臺大封校",
+                "臺大封校影像紀錄",
                 style: TextStyle(
                     fontSize: 20, fontFamily: 'NotoSans', color: Colors.black),
               ),
